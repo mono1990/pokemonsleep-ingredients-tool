720849e9d4d814f81e66b01b44269d640e762b67
ono <m.ono1990bolta@gmail.com>
2025-04-10 00:17:49 +0900

    20250410

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sleepblue: '#8CC6E7',
+        sleepyello: '#FFF4CC',
+        sleepgreen: '#C8E6C9',
+        sleeppink: '#F8BBD0',
+      }
+    },
   },
   plugins: [],
-  extend: {
-    colors: {
-      sleepblue: '#8CC6E7',
-      sleepyello: '#FFF4CC',
-      sleepgreen: '#C8E6C9',
-      sleeppink: '#F8BBD0',
-    }
-  }  
 }
 
